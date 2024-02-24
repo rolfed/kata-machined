66141fd1c3f753835dc42a15192f8a3c8bca3d61
Danniel Rolfe <danniel.rolfe@experian.com>
2024-02-23 16:21:24 -0800

    WIP leetcode search algorigthm

--- src/leetcode/search/search_insert_position.ts
+++ src/leetcode/search/search_insert_position.ts
@@ -5,72 +5,74 @@
 
     You must write an algorithm with O(log n) runtime complexity.
 
+    Example 0:
+
+    Input: nums = [0,3,5,6], target = 5
+    Output: 2 
     Example 1:
 
-    Input: nums = [1,3,5,6], target = 5
-    Output: 2
+    Input: nums = [0,3,5,6], target = 2
+    Output: 1 
     Example 2:
 
-    Input: nums = [1,3,5,6], target = 2
-    Output: 1
-    Example 3:
-
-    Input: nums = [1,3,5,6], target = 7
-    Output: 4
+    Input: nums = [0,3,5,6], target = 7
+    Output: 4 
 **/
 
 function searchInsert(nums: number[], target: number): number {
     let lo = 0; // inclusive
     let hi = nums.length; // exclusive
-    let midway = Math.floor(lo + (hi - lo) / 2);
     let location = -1; // not found
+    let midway = Math.floor(lo + (hi - lo) / 2);
 
     do {
         let value = nums[midway];
-        if (value === target) {
+        if (target === value) {
             location = nums.indexOf(value);
-            console.log('loc ', location);
         } else if (value > target) { // right side
             lo = midway + 1;
         } else {
             hi = midway;
-        }
+        } 
 
-            console.log('l noc ', location);
-        if (!!location) {
-            console.log('location not found');
-            // target >= nums[i] && target < nums[i + 1] get index of nums[i + 1] 
-            const next = nums.indexOf(value) + 1;
-            if (value >= target && value < nums[next]) {
-                location = next;
-            }   
-
+        if (location === -1) {
+            // bigger than last index
+            if (target > nums[nums.length - 1]) {
+                location = nums.length;
+                console.log('last: ', location);
+            } else {
+                midway = Math.floor(lo + (hi - lo) / 2);
+                value = nums[midway];
+                lo = 0;
+                hi = nums.length;
+                location = 0;
+            }
         }
-    } while (lo > hi || !location);
+    } while (lo > hi && location === -1);
 
     return location;
 }
 
 function oneTestSearchInsert(): boolean {
-    const actualResult = searchInsert([1, 3, 5, 6], 5);
+    const actualResult = searchInsert([0, 3, 5, 6], 5);
     const expectedResult = 2;
     return actualResult === expectedResult
 };
 
 function twoTestSearchInsert(): boolean {
-    const actualResult = searchInsert([1, 3, 5, 6], 2);
+    const actualResult = searchInsert([0, 3, 5, 6], 2);
     const expectedResult = 1;
     return actualResult === expectedResult
 };
 
 function threeTestSearchInsert(): boolean {
-    const actualResult = searchInsert([1, 3, 5, 6], 7);
-    const expectedResult = 1;
+    const actualResult = searchInsert([0, 3, 5, 6], 7);
+    const expectedResult = 4;
     return actualResult === expectedResult
 };
 
 console.log(
-//oneTestSearchInsert(),
+oneTestSearchInsert(),
 twoTestSearchInsert(),
-// threeTestSearchInsert()
+threeTestSearchInsert()
 );
